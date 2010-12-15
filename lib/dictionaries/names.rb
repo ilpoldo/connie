f1e6413684201dfba1162b0b680cbc73a2c77371
ilpoldo <ilpoldo@gmail.com>
2010-12-15 00:12:21 +0000

    polishing the name generator

--- lib/dictionaries/names.rb
+++ lib/dictionaries/names.rb
@@ -18,5 +18,15 @@ module Connie
     def first_with_title
       Connie? ? [male_title, male] : [female_title, female]
     end
+    
+    
+    def first_sometimes_with_title
+      if Connie?
+        Connie? ? [male_title, male] : [female_title, female]
+      else
+        Connie? ? [nil, male] : [nil, female]
+      end
+    end
+    
   end
 end
