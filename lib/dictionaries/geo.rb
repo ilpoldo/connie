9989234936989e82d2d56db7ccc9543db0011da4
ilpoldo <ilpoldo@gmail.com>
2011-01-21 09:38:02 +0000

    fix a space parenthesis warning

--- lib/dictionaries/geo.rb
+++ lib/dictionaries/geo.rb
@@ -2,7 +2,7 @@ module Connie
   module Geo
 
     def full_address
-      "#{Connie (1..500)}, #{street} #{street_suffix} \n #{city}, #{country}"
+      "#{Connie(1..500)}, #{street} #{street_suffix} \n #{city}, #{country}"
     end
 
   end
